21b4b2c9a4b85154f831cb48b416c412d8a3e883
MWLeonel <wmleonel6@gmail.com>
2020-09-25 00:52:01 -0300

    Nome em Desenho

---
+++ NomeAsciiArt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Figgle;
+
+namespace NomeAsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string SeuNome;
+
+            Console.Write("Digite seu nome: ");
+            SeuNome = FiggleFonts.Basic.Render(Console.ReadLine());
+            Console.Write("\n");
+
+            Console.WriteLine("Seu nome usando figgle é assim");
+            Console.Write("\n");
+            Console.WriteLine(SeuNome);
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
